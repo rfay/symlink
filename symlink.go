9c40e9a7fdd6b30fb83cea3e34a697232d515d13
Randy Fay <randy@randyfay.com>
2018-11-28 19:52:01 -0700

    Symlink

---
+++ symlink.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	from := "symlink.go"
+	to := "thisisasymlink.link"
+	err := os.Symlink(from, to)
+	if (err != nil) {
+		fmt.Printf("Failed to symlink: %v\n", err)
+	}
+}
